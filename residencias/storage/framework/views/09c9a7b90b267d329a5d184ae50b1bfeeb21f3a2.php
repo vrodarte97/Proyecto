2dfd103887f88c2e4380acce39730cf111762d93
vrodarte97 <67845826+vrodarte97@users.noreply.github.com>
2021-02-24 18:59:24 -0700

    update

---
+++ residencias/storage/framework/views/09c9a7b90b267d329a5d184ae50b1bfeeb21f3a2.php
@@ -0,0 +1,78 @@
+
+<p><Strong>INSTRUCCIONES: <br></Strong>
+    <br>
+    <u style="color:green">Por favor, rellena los campos que se muestran a continuación SIN ACENTOS.</u>
+    </p>
+    
+    <div class="form-group">
+    <label for="NoCtrl" class="control-label"><?php echo e('Número de Control: '); ?></label>
+    <input class="form-control <?php echo e($errors->has('NoCtrl')?'is-invalid':''); ?> " type="number" name="NoCtrl" id="NoCtrl" min="9"
+    value="<?php echo e(isset($alumno->NoCtrl)?$alumno->NoCtrl:old('NoCtrl')); ?>"
+    > 
+
+        <?php echo $errors->first('NoCtrl','<div class="invalid-feedback">:message</div>' ); ?>
+
+        
+
+    </div>
+    
+    <div class="form-group">
+    <label for="Nombre" class="control-label"><?php echo e('Nombre(s): '); ?></label>
+    <input class="form-control <?php echo e($errors->has('Nombre')?'is-invalid':''); ?> " type="text" name="Nombre" id="Nombre"  onKeyUp="document.getElementById(this.id).value=document.getElementById(this.id).value.toUpperCase()"
+    value="<?php echo e(isset($alumno->Nombre)?$alumno->Nombre:old('Nombre')); ?>"
+    > 
+    <?php echo $errors->first('Nombre','<div class="invalid-feedback">:message</div>' ); ?>
+
+
+    </div>
+    
+    <div class="form-group">
+    <label for="Apaterno" class="control-label"><?php echo e('Apellido Paterno: '); ?></label>
+    <input class="form-control <?php echo e($errors->has('Apaterno')?'is-invalid':''); ?> " type="text" name="Apaterno" id="Apaterno"  onKeyUp="document.getElementById(this.id).value=document.getElementById(this.id).value.toUpperCase()"
+    value="<?php echo e(isset($alumno->Apaterno)?$alumno->Apaterno:old('Apaterno')); ?>"
+    > 
+    <?php echo $errors->first('Apaterno','<div class="invalid-feedback">:message</div>' ); ?>
+
+
+    </div>
+    
+    <div class="form-group">
+    <label for="Amaterno" class="control-label"><?php echo e('Apellido Materno: '); ?></label>
+    <input class="form-control <?php echo e($errors->has('Amaterno')?'is-invalid':''); ?> " type="text" name="Amaterno" id="Amaterno"  onKeyUp="document.getElementById(this.id).value=document.getElementById(this.id).value.toUpperCase()" 
+    value="<?php echo e(isset($alumno->Amaterno)?$alumno->Amaterno:old('Amaterno')); ?>"
+    > 
+    <?php echo $errors->first('Amaterno','<div class="invalid-feedback">:message</div>' ); ?>
+
+
+    </div>
+    
+    <div class="form-group">
+    <label for="Carrera" class="control-label <?php echo e($errors->has('Carrera')?'is-invalid':''); ?> "><?php echo e('Carrera: '); ?></label>
+
+    <select name="Carrera" class="form-control <?php echo e($errors->has('NoCtrl')?'is-invalid':''); ?> ">
+                    
+        <OPTION VALUE="<?php echo e(isset($alumno->Carrera)?$alumno->Carrera:''); ?>"><?php echo e(isset($alumno->Carrera)?$alumno->Carrera:old('Carrera')); ?></OPTION>
+        <OPTION VALUE="ADMINISTRACION">ADMINISTRACI&Oacute;N</OPTION>
+        <OPTION VALUE="CONTADOR">CONTADOR</OPTION>
+        <OPTION VALUE="ELECTROMECANICA">ELECTROMEC&Aacute;NICA</OPTION>
+        <OPTION VALUE="MECANICA">MEC&Aacute;NICA</OPTION>
+        <OPTION VALUE="SISTEMAS">SISTEMAS</OPTION>
+        <OPTION VALUE="ELECTRONICA">ELECTR&Oacute;NICA</OPTION>
+        <OPTION VALUE="TICS">TICS</OPTION>
+        <OPTION VALUE="IGE">IGE</OPTION>
+        <OPTION VALUE="ELECTRICA">EL&Eacute;CTRICA</OPTION>
+        <OPTION VALUE="INDUSTRIAL">INDUSTRIAL</OPTION>
+        <OPTION VALUE="MECATRONICA">MECATR&Oacute;NICA</OPTION>
+    
+    </select>
+
+    <?php echo $errors->first('Carrera','<div class="invalid-feedback">:message</div>' ); ?>
+
+
+
+    </div>
+
+    <p><strong>¿ES CORRECTA LA CARRERA?</strong></p>
+
+    <input type="submit" class="btn btn-primary" value="<?php echo e($Modo=='crear' ? 'Agregar':'Modificar'); ?>">
+    <a class="btn btn-danger" href="<?php echo e(url('alumnos')); ?>">Cancelar</a><?php /**PATH C:\wamp64\www\resi\residencias\resources\views/alumnos/form.blade.php ENDPATH**/ ?>
---
+++ residencias/storage/framework/views/fb7c0c85e91582b01a15e36c2b0ccad31740d3ec.php
@@ -0,0 +1,83 @@
+
+
+<?php $__env->startSection('content'); ?>
+
+<div class="container">
+
+<?php if(Session::has('Mensaje')): ?>
+<div class="alert alert-success" role="alert">
+    <?php echo e(Session::get('Mensaje')); ?>
+
+</div>
+
+<?php endif; ?>
+<br>
+
+
+
+
+<nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <a href="<?php echo e(url('alumnos/create')); ?>" class="btn btn-success">Agregar Alumno</a>
+      
+        <form class="d-flex" style="display:inline">
+        <input name= "buscarpor" class="form-control me-2" type="search" placeholder="Buscar por apellido" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      <form>
+    </div>
+  </nav>
+
+<br><br>
+
+<table class="table table-light table-hover">
+
+    <thead class="thead-light">
+        <tr>
+            <th>#</th>
+            <th>N&uacute;mero de control</th>
+            <th>Nombre(s)</th>
+            <!--Si voy a usar una solo columa comentar Apa, Ama-->
+            <th>Apellido Paterno</th>
+            <th>Apellido Materno</th>
+            <th>Carrera</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $__currentLoopData = $alumnos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $alumno): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            
+        <tr>
+            <td><?php echo e($loop->iteration); ?></td>
+            <td><?php echo e($alumno->NoCtrl); ?></td>
+            <td><?php echo e($alumno->Nombre); ?></td>
+            
+            <td><?php echo e($alumno->Apaterno); ?></td>
+            <td><?php echo e($alumno->Amaterno); ?></td>
+            <td><?php echo e($alumno->Carrera); ?></td>
+            <td>
+
+            <a class="btn btn-outline-success" href="<?php echo e(url('/alumnos/'.$alumno->id.'/edit')); ?>">
+            Editar
+            </a>
+                
+                <form method="post" action="<?php echo e(url('/alumnos/'.$alumno->id)); ?>" style="display:inline">
+                <?php echo e(csrf_field()); ?>
+
+                <?php echo e(method_field('DELETE')); ?>
+
+                <button class="btn btn-outline-danger" type="submit" onclick="return confirm('¿Estas seguro que deseas eliminar este dato? Una vez eliminado no podras recuperarlo.')">Borrar </button>
+                </form> 
+            </td>
+        </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </tbody>
+</table>
+
+
+
+</div>
+
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\wamp64\www\resi\residencias\resources\views/alumnos/index.blade.php ENDPATH**/ ?>
